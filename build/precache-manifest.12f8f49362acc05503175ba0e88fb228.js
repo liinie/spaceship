578c52fcaf8c6a480103c157063d6952ee4b31cc
Lin Xu <Lin.Xu@campus.lmu.de>
2018-11-30 10:11:17 +0100

    changed settings and .gitignore

---
+++ build/precache-manifest.12f8f49362acc05503175ba0e88fb228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spaceship/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0554db6feb414ac33052",
+    "url": "/spaceship/static/js/runtime~main.0554db6f.js"
+  },
+  {
+    "revision": "d4942867d09762fda90c",
+    "url": "/spaceship/static/js/main.d4942867.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/spaceship/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "d4942867d09762fda90c",
+    "url": "/spaceship/static/css/main.ac81641a.chunk.css"
+  },
+  {
+    "revision": "148b2c47b29bafa6c8d21b2b3c638541",
+    "url": "/spaceship/index.html"
+  }
+];
